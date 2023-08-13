e8cf9e51da94d9df699543abfd149d06cb8593ee
supercaliflipette <cverein.professionnel@yahoo.fr>
2023-08-13 22:40:08 +0200

    feat(timetable): add method to fix taiwind classes

--- components/timetable/TimetableEvent.tsx
+++ components/timetable/TimetableEvent.tsx
@@ -6,6 +6,25 @@ type EventProps = {
 
   export function TimetableEvent({event}:EventProps) {
 
+    function getBgColorFromColorId(colorId:string): string {
+      switch(colorId) {
+        case  'bg-blue-200':
+        return 'bg-blue-200'
+        case 'bg-purple-200':
+          return 'bg-purple-200'
+          case 'bg-red-300':
+          return 'bg-red-300'
+          case 'bg-yellow-400':
+          return 'bg-yellow-400'
+          case 'bg-orange-200':
+          return 'bg-orange-200'
+          case 'bg-red-200':
+          return 'bg-red-200'
+      }
+
+
+        return ''
+    }
     return (
       <div className={`flex flex-col rounded-lg p-4 ${event.color}`}>
         <div className={`font-bold`}>
